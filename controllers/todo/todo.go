2f4bc05cd69f1e8bc91fea35c1d4a9839c0add18
Abdulqudduus <babalolajnr@gmail.com>
2022-09-29 14:34:25 +0100

    feat: implement get todos

--- controllers/todo/todo.go
+++ controllers/todo/todo.go
@@ -8,7 +8,20 @@ import (
 )
 
 func Todos(c *fiber.Ctx) error {
-	panic("Not implemented yet")
+
+	user, err := auth.AuthenticatedUser(c)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "error", "message": "Could not find user", "data": nil})
+	}
+
+	var todos []models.Todo
+
+	db := database.DB.Db
+
+	db.Preload("User").Where("user_id = ?", user.ID).Find(&todos)
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success", "message": "Todos retrieved", "data": todos})
 }
 
 func Create(c *fiber.Ctx) error {
